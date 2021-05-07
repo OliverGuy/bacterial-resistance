bbdf7118f73228abc3b20e7a280d5fbc6fdd18de
Arthur Cahu <arthur.cahu@gmail.com>
2021-05-07 13:47:10 +0200

    subclassed model
    also cleaned up some lines

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 
-import h5py  # this is used to save Keras models in the hdf5 format
 import datetime
 import os
 import numpy as np
 import pandas as pd
-import sys
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # suppress tf info-level logs
 # imports for Keras
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Input, Dense, Conv1D, Dropout, Flatten, MaxPooling1D, Embedding
+from model import CNNModel
 
 from preprocessing import classes
 from contigParser import nucleotides
@@ -20,12 +18,13 @@ from contigDataset import ContigDataGenerator
 def main():
 
     # a few hard-coded values
-    sequence_length = 1299315  # length of a DNA/RNA sequence for the bacteria, None=inferred
-    voc_size = len(nucleotides)  # 5:ACGTN
-    batch_size = 50
-    epochs = 1000  # 500
+    voc_size = len(nucleotides)   # 5:NACGT
+    batch_size = 16
+    epochs = 500
     n_folds = 10
     parser = "cut"  # cf. contigParser.py
+    # this is used to stop and restart the testing on a sequence of folds
+    # (only works with a fixed random state)
     starting_fold = 0
     # TODO change to None for pseudo-random number generation initialized with time:
     random_state = 42
@@ -61,7 +60,9 @@ def main():
     # data pre-processing
     # prepare data in the correct format
     print("Reading data...")
-    ast_data = pd.read_csv(os.path.join(contig_folder, "ast.csv"), header=0, index_col=0)
+    ast_data = pd.read_csv(os.path.join(contig_folder, "ast.csv"),
+                           header=0,
+                           index_col=0)
 
     antibiotic = "gentamicin"
 
@@ -74,122 +75,28 @@ def main():
     # integer-encode classes
     y = ast_data[antibiotic].replace(classes).to_numpy()
 
-    number_of_classes = len(np.unique(y))  # 2
+    n_classes = len(np.unique(y))  # 2
     print(f"Considering response to {antibiotic}")
     print(f"Number of samples: {ast_data.shape[0]}, {voc_size} nucleotides")
-    print(f"{number_of_classes} unique classes: {np.unique(y)}")
+    print(f"{n_classes} unique classes: {np.unique(y)}")
 
     # dataset generator parameters
     generator_params = {
         "folder": contig_folder,
+        "n_classes": n_classes,
         "parser": parser,
         "batch_size": batch_size,
         "shuffle": True,
         "random_state": random_state
     }
-    # length of output contigs must be consistent for the dense layer
-    if not sequence_length:
-        sequence_length = ContigDataGenerator(X, y, **generator_params).compute_sequence_length()
-    generator_params["sequence_length"] = sequence_length
-    print(f"sequence length: {sequence_length}")
-
-    # shapes of the tensors in the network
-    # (?, 1299315, 1)
-    # (?, 210, 130)
-    # (?, 1, 204)
-    # (?, 1, 150)
-    # (?, 5)
-
-    # input
-    # python3 gen3.py 500 130 204 150 196 148 236 81 9 106 121 0 0 0
-
-    # default values for the network
-    embed_dim = 1
-    # in order NACGT, see nucleotides:
-    embed_init_w = np.array([0, 1, 0.25, 0.75, 0.5]).reshape((-1, 1))
-
-    units_1 = 12  # 130
-    units_2 = 204
-    units_3 = 150
-    units_4 = 196
-    pool_size_1 = 148
-    pool_size_2 = 236
-    pool_size_3 = 81
-    conv_window_length_1 = 21
-    conv_window_length_2 = 106
-    conv_window_length_3 = 121
-    dropout_probability = 0.5
-
-    # default value for bias
-    bias_vector_init_value = 0.1
-
-    # the loss is adjusted with a parameter for l2 regularization in each of
-    # the convolutional layers, called "beta" in the gen3.py script
-    penalty_regularization_w_conv = 0.001
 
     # create the network
     print("Creating network...")
-    network = Sequential()
 
-    network.add(Input(
-        shape=(sequence_length,)
-    ))
-    print(network.input_shape)
-    # embeds integral indices into dense arrays
-    # TODO also try with a one-hot encoding
-    embed_layer = Embedding(voc_size, embed_dim)
-    network.add(embed_layer)
-    network.get_layer("embedding").set_weights([embed_init_w])
-    # freeze the model to keep embeddings constant:
-    # network.get_layer("embedding").trainable = False
-    print(network.output_shape)
-
-
-    # using conv1D instead of resizing the data to make it fit conv2D
-    # 1 Layer
-    network.add(Conv1D(
-        units_1,
-        activation='relu',
-        kernel_size=conv_window_length_1,
-        padding='same',
-        use_bias=True,
-        bias_initializer=tf.keras.initializers.Constant(bias_vector_init_value),
-        kernel_regularizer=tf.keras.regularizers.l2(penalty_regularization_w_conv)))
-    print(network.output_shape)
-    network.add(MaxPooling1D(pool_size=pool_size_1, padding='same'))
-    print(network.output_shape)
-
-    # 2 Layer
-    #network.add(Conv1D(units_2, activation='relu', kernel_size=conv_window_length_2, padding='same', use_bias=True, bias_initializer=keras.initializers.Constant(bias_vector_init_value), kernel_regularizer=keras.regularizers.l2(penalty_regularization_w_conv)))
-    #network.add(MaxPooling2D(pool_size=pool_size_2, padding='same'))
-
-    # 3 Layer
-    #network.add(Conv1D(units_3, activation='relu', kernel_size=conv_window_length_3, padding='same', use_bias=True, bias_initializer=keras.initializers.Constant(bias_vector_init_value), kernel_regularizer=keras.regularizers.l2(penalty_regularization_w_conv)))
-    #network.add(MaxPooling2D(pool_size=pool_size_3, padding='same'))
-
-    # Rectifier Layer, with dropout
-    network.add(Flatten())  # convert tensor in N dimensions to tensor in 2
-    print(network.output_shape)
-    network.add(Dense(
-        units_4,
-        activation='relu',
-        use_bias=True,
-        bias_initializer=tf.keras.initializers.Constant(bias_vector_init_value)))
-    network.add(Dropout(dropout_probability))
-    print(network.output_shape)
-
-    # (Output) Softmax
-    network.add(Dense(
-        number_of_classes,
-        activation='softmax',
-        use_bias=True,
-        bias_initializer=tf.keras.initializers.Constant(bias_vector_init_value)
-    ))
-    print(network.output_shape)
+    network = CNNModel(voc_size=voc_size, n_classes=n_classes)
 
     # instantiate the optimizer, with its learning rate
-    from keras.optimizers import Adam
-    optimizer = Adam(learning_rate=1e-5)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5)
 
     # the loss is categorical crossentropy, as this is a classification problem
     # TODO use binary crossentropy for multi-label classification and account for unknown classes
@@ -197,15 +104,16 @@ def main():
                     loss='categorical_crossentropy',
                     metrics=['categorical_accuracy'])
 
-    print("Network has been successfully compiled.")
-    network.summary()
+    print("Building network...")
+    # calling model to build it
 
-    # TODO check layer layout
-    from tensorflow.keras.utils import plot_model
-    plot_model(
-        network,
-        to_file= os.path.join(output_folder, "model.png"),
-        show_shapes=True)
+    network.predict(ContigDataGenerator(X[:batch_size],
+                                        y[:batch_size],
+                                        **generator_params))
+
+    # freeze the layer to keep embeddings constant:
+    # network.get_layer("embedding").trainable = False
+    network.summary()
 
     # a 'callback' in Keras is a condition that is monitored during the training process
     # here we instantiate a callback for an early stop, that is used to avoid overfitting
@@ -219,7 +127,8 @@ def main():
 
     # before going into the cross-validation, we actually save the initial
     # random weights of the network, to reset it later before each fold
-    network.save_weights(os.path.join(output_folder, "initial-random-weights.h5"))
+    network.save_weights(os.path.join(
+        output_folder, "initial-random-weights.h5"))
 
     #  cross-validation
     # TODO adapt stratification to multi-label classification
@@ -240,20 +149,27 @@ def main():
         y_train_and_val, y_test = y[train_and_val_index], y[test_index]
 
         # get test and validation set indexes, using a StratifiedShuffleSplit with just one split
-        validation_shuffle_split = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=random_state)
-        train_index, val_index = next(validation_shuffle_split.split(X_train_and_val, y[train_and_val_index]))
+        validation_shuffle_split = StratifiedShuffleSplit(
+            n_splits=1, test_size=0.1, random_state=random_state)
+        train_index, val_index = next(validation_shuffle_split.split(
+            X_train_and_val, y[train_and_val_index]))
 
         X_train, X_val = X_train_and_val[train_index], X_train_and_val[val_index]
         y_train, y_val = y_train_and_val[train_index], y_train_and_val[val_index]
 
-        fold_report = f"Fold {fold}/{n_folds} (samples train={len(X_train)}, validation={len(X_val)}, test={len(X_test)}"
+        fold_report = f"Fold {fold+1}/{n_folds} (samples train={len(X_train)}, validation={len(X_val)}, test={len(X_test)}"
 
         print(fold_report + ": starting the training process...")
 
-        training_generator = ContigDataGenerator(X_train, y_train, **generator_params)
-        validation_generator = ContigDataGenerator(X_val, y_val, **generator_params)
-        testing_generator = ContigDataGenerator(X_test, y_test, **generator_params)
-        network.load_weights(os.path.join(output_folder, "initial-random-weights.h5"))  # reset network to initial state
+        training_generator = ContigDataGenerator(
+            X_train, y_train, **generator_params)
+        validation_generator = ContigDataGenerator(
+            X_val, y_val, **generator_params)
+        testing_generator = ContigDataGenerator(
+            X_test, y_test, **generator_params)
+        # reset network to initial state
+        network.load_weights(os.path.join(
+            output_folder, "initial-random-weights.h5"))
 
         train_history = network.fit(
             training_generator,
@@ -272,16 +188,7 @@ def main():
         )
 
         print("Training process finished. Testing...")
-        # TODO generators, evaluate
-        # y_train_pred = network.predict(X_train).argmax(axis=1)
-        # y_val_pred = network.predict(X_val).argmax(axis=1)
-        # y_test_pred = network.predict(X_test).argmax(axis=1)
-
-        # TODO use keras.metrics.CategoricalAccuracy for one-hot labels instead
-        # from sklearn.metrics import accuracy_score
-        # train_accuracy = accuracy_score(y_train, y_train_pred)
-        # val_accuracy = accuracy_score(y_val, y_val_pred)
-        # test_accuracy = accuracy_score(y_test, y_test_pred)
+        # TODO to be tested; training doesn't reach that stage in reasonable time yet
 
         print(train_history.history.keys())
 
@@ -302,7 +209,8 @@ def main():
         network_json = network.to_json()
         with open(os.path.join(output_folder, f"fold-{fold}-model.json"), "w") as fp:
             fp.write(network_json)
-        network.save_weights(os.path.join(output_folder, f"fold-{fold}-weights.h5"))
+        network.save_weights(os.path.join(
+            output_folder, f"fold-{fold}-weights.h5"))
 
         # save information about the fold
         with open(os.path.join(output_folder, "global-summary.txt"), "a") as fp:
@@ -335,4 +243,4 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
---
+++ model.py
@@ -0,0 +1,217 @@
+
+"""
+Defines the models and layers used in the NN.
+Includes most layer parameters as well.
+"""
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Conv1D, Dropout, GlobalMaxPooling1D, Embedding
+from tensorflow.keras.layers.experimental.preprocessing import Resizing
+
+# default values for the network
+
+embed_dim = 1
+# in order NACGT, see nucleotides:
+embed_init_w = [0, 1, 0.25, 0.75, 0.5]
+
+sequence_target_length = 100
+interpolation = "bilinear"
+
+conv_filters_1 = 12  # 130
+units_2 = 204
+units_3 = 150
+dense_units_1 = 64  # 196
+pool_size_1 = 148
+pool_size_2 = 236
+pool_size_3 = 81
+conv_window_length_1 = 31
+conv_window_length_2 = 106
+conv_window_length_3 = 121
+dropout_probability = 0.5
+
+# default value for bias
+bias_vector_init_value = 0.1
+
+# the loss is adjusted with a parameter for l2 regularization in each of
+# the convolutional layers
+penalty_regularization_w_conv = 0.001
+
+# 2 Layer
+# network.add(Conv1D(units_2, activation='relu', kernel_size=conv_window_length_2, padding='same', use_bias=True, bias_initializer=keras.initializers.Constant(bias_vector_init_value), kernel_regularizer=keras.regularizers.l2(penalty_regularization_w_conv)))
+# network.add(MaxPooling2D(pool_size=pool_size_2, padding='same'))
+
+# 3 Layer
+# network.add(Conv1D(units_3, activation='relu', kernel_size=conv_window_length_3, padding='same', use_bias=True, bias_initializer=keras.initializers.Constant(bias_vector_init_value), kernel_regularizer=keras.regularizers.l2(penalty_regularization_w_conv)))
+# network.add(MaxPooling2D(pool_size=pool_size_3, padding='same'))
+
+
+@tf.function(input_signature=[tf.TensorSpec(shape=(None, None), dtype=tf.bool)])
+def masked_lengths(mask):
+    """Computes the length of each sequence using 2D mask data; essentially
+    the inverse of `tf.sequence_mask`."""
+    return tf.shape(mask)[1] - tf.math.argmax(mask[:, ::-1], axis=1, output_type=tf.int32)
+
+
+class Resizing1D(tf.keras.layers.Layer):
+    def __init__(self, length=1000, interpolation="bilinear", name="resizing1d", **kwargs):
+        super(Resizing1D, self).__init__(name=name, **kwargs)
+        self.resizer = Resizing(1, length, interpolation=interpolation)
+
+    def __resize_with_length(self, t):
+        sequence, length = t
+        # assumes row shape: batch(1) * height(1) * width * channel:
+        tf.debugging.assert_shapes([
+            (sequence, (1, 1, "W", "C")),
+            (length, (1,))
+        ])
+        return self.resizer(sequence[:, :, :length, :])
+
+    def call(self, inputs, mask=None):
+        # input shape : num_nodes * node_length * embed_dim
+        # add a dummy height dimension to all sequences:
+        x = tf.expand_dims(inputs, axis=1)
+        if mask is not None:
+            lengths = masked_lengths(mask)
+            # add a dummy batch dimension to all sequences:
+            x = tf.expand_dims(x, axis=1)
+            # output signature is expected to be same as input unless otherwise noted
+            x = tf.map_fn(
+                self.__resize_with_length,
+                (x, lengths),
+                fn_output_signature=x.dtype
+            )
+            # remove dummy dimensions:
+            return tf.squeeze(x, axis=[1, 2])
+            # return self.__resize_with_lengths(x, lengths)
+        else:
+            return tf.squeeze(self.resizer(x), axis=[1])
+            # (remove dummy height dimension)
+
+    def get_config(self):
+        config = super(Resizing1D, self).get_config()
+        resize_config = self.resizer.get_config()
+        config.update({
+            "interpolation": resize_config["interpolation"],
+            "length": resize_config["width"]
+        })
+        return config
+
+
+class CNNModel(tf.keras.Model):
+
+    def __init__(self, voc_size=5, n_classes=2):
+        super(CNNModel, self).__init__()
+        self.n_classes = n_classes
+        # embeds integral indices into dense arrays
+        # TODO also try with a one-hot encoding
+        self.embed_layer = Embedding(
+            voc_size,
+            embed_dim,
+            mask_zero=True,
+            embeddings_initializer=lambda shape, dtype=None:
+                tf.convert_to_tensor(
+                    np.array(embed_init_w).reshape(shape),
+                    dtype=dtype
+                )
+        )
+
+        self.resizer = Resizing1D(
+            length=sequence_target_length,
+            interpolation="bilinear"
+        )
+
+        self.conv1 = Conv1D(
+            conv_filters_1,
+            kernel_size=conv_window_length_1,
+            input_shape=(None, embed_dim),
+            activation='relu',
+            padding='valid',
+            data_format="channels_last",
+            use_bias=True,
+            bias_initializer=tf.keras.initializers.Constant(
+                bias_vector_init_value),
+            kernel_regularizer=tf.keras.regularizers.l2(
+                penalty_regularization_w_conv)
+        )
+        self.globalmaxpool = GlobalMaxPooling1D()
+
+        # Rectifier Layer, with dropout
+        self.dense1 = Dense(
+            dense_units_1,
+            activation='relu',
+            use_bias=True,
+            bias_initializer=tf.keras.initializers.Constant(
+                bias_vector_init_value)
+        )
+        self.dropout = Dropout(dropout_probability)
+
+        # Classification head with softmax
+        self.classifier = Dense(
+            n_classes,
+            activation='softmax',
+            use_bias=True,
+            bias_initializer=tf.keras.initializers.Constant(
+                bias_vector_init_value)
+        )
+
+        # dropout won't be built on predict:
+        self.dropout.build((None, dense_units_1))
+
+    def call(self, inputs, training=None):
+        """Returns y_pred as a `inputs.shape[0] * n_classes` tensor.
+
+        Input shape is assumed to be `batch_size * num_nodes *
+        node_length`, ie the output of a `contigParser` with `ndims=2`. 
+
+        Called by `__call__`, `predict`, `fit` and so on.
+        """
+        # input shape: batch_size * num_nodes * node_length
+        # the tensorflow equivalent of a list, allows backpropagation:
+        results = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
+        # apply the model on each contig independently:
+        for idx, contig in enumerate(inputs):
+            tf.debugging.assert_shapes([
+                (contig, ("num_nodes", "node_length"))
+            ])
+            # embed each integer-represented nucleotide:
+            x = self.embed_layer(contig)
+            tf.debugging.assert_shapes([
+                (x, ("num_nodes", "node_length", self.embed_layer.output_dim))
+            ])
+            # resize nodes:
+            x = self.resizer(x)
+            tf.debugging.assert_shapes([
+                (x, ("num_nodes", self.resizer.resizer.target_width,
+                 self.embed_layer.output_dim))
+            ])
+            # convolve over node lengths:
+            x = self.conv1(x)
+            tf.debugging.assert_shapes([
+                (x, ("num_nodes", "node_length_resized", self.conv1.filters))
+            ])
+            # take the max along each node:
+            x = self.globalmaxpool(x)
+            tf.debugging.assert_shapes([
+                (x, ("num_nodes", self.conv1.filters))
+            ])
+            x = self.dense1(x)
+            tf.debugging.assert_shapes([
+                (x, ("num_nodes", self.dense1.units))
+            ])
+            if training:
+                x = self.dropout(x)
+            x = self.classifier(x)
+            tf.debugging.assert_shapes([
+                (x, ("num_nodes", self.n_classes))
+            ])
+            # now vote by averaging the predictions from each node:
+            # (NB: need to reshape since we took out the batch dimension)
+            x = self.globalmaxpool(tf.expand_dims(x, axis=0))
+            # 1 * 2
+            tf.debugging.assert_shapes([
+                (x, (1, self.n_classes))
+            ])
+            # squeezing to scrap the dummy batch dimension:
+            results = results.write(idx, tf.squeeze(x))
+        return results.stack()
